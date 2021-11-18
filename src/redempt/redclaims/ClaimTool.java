42dd3aacc80fc6e542db3a66333fa441b9a5a74f
Redempt <instantiable@gmail.com>
2021-11-18 13:45:27 -0500

    Improvements to claim tool - Now dynamically displays "arms" of claim to indicate which direction the corner is in - Claim tool will automatically visualize the claim you are in while held

--- src/redempt/redclaims/ClaimTool.java
+++ src/redempt/redclaims/ClaimTool.java
@@ -6,11 +6,13 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
@@ -27,6 +29,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 
@@ -81,13 +84,38 @@ public class ClaimTool implements Listener {
 	
 	@EventHandler
 	public void onHotbarSwap(PlayerChangedHeldItemEvent e) {
-		if (!ItemUtils.compare(e.getNewItem(), item)) {
+		boolean cur = ItemUtils.compare(e.getNewItem(), item);
+		boolean prev = ItemUtils.compare(e.getPreviousItem(), item);
+		System.out.println(cur + " " + prev);
+		if (!cur && !prev) {
 			return;
 		}
 		Claim claim = ClaimMap.getClaim(e.getPlayer().getLocation());
-		if (claim != null) {
-			claim.visualize(e.getPlayer(), false);
+		if (claim == null) {
+			return;
 		}
+		if (cur) {
+			claim.visualize(e.getPlayer(), false);
+			return;
+		}
+		claim.unvisualize(e.getPlayer());
+	}
+	
+	@EventHandler
+	public void onMove(PlayerMoveEvent e) {
+		if (!ItemUtils.compare(item, e.getPlayer().getInventory().getItemInMainHand())) {
+			return;
+		}
+		Claim to = ClaimMap.getClaim(e.getTo());
+		Claim from = ClaimMap.getClaim(e.getFrom());
+		if (Objects.equals(to, from)) {
+			return;
+		}
+		if (to == null) {
+			from.unvisualize(e.getPlayer());
+			return;
+		}
+		to.visualize(e.getPlayer(), false);
 	}
 	
 	@EventHandler
@@ -190,16 +218,28 @@ public class ClaimTool implements Listener {
 			return OverlapResult.OVERLAPS;
 		}
 		
-		private void showCorners(String message, Location loc1, Location loc2, Material block) {
+		private final BlockFace[] faces = {BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST};
+		
+		private void showCorners(String message, Location loc1, Location loc2, Material type) {
 			player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(message));
 			List<Location> locs = new ArrayList<>();
+			CuboidRegion region = new CuboidRegion(loc1, loc2).expand(1, 0, 1, 0, 1, 0);
 			locs.add(new Location(loc1.getWorld(), loc1.getX(), 0, loc2.getZ()));
 			locs.add(new Location(loc1.getWorld(), loc2.getX(), 0, loc1.getZ()));
 			locs.forEach(l -> l.setY(l.getWorld().getHighestBlockYAt(l)));
 			locs.add(loc1);
 			locs.add(loc2);
 			for (Location loc : locs) {
-				displayer.display(loc.getBlock(), block);
+				displayer.display(loc.getBlock(), type);
+				Block block = loc.getBlock();
+				for (BlockFace face : faces) {
+					Block rel = block.getRelative(face);
+					if (!region.contains(rel)) {
+						continue;
+					}
+					rel = rel.getWorld().getHighestBlockAt(rel.getLocation());
+					displayer.display(rel, Material.WAXED_OXIDIZED_CUT_COPPER);
+				}
 			}
 			displayer.show();
 		}
