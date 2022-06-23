62474a3d3e19127e588c0f72fadec225f81b4eb6
UntouchedOdin0 <UntouchedOdin0@users.noreply.github.com>
2022-06-23 10:34:34 +0100

    Update ClaimExpansion.java

--- src/redempt/redclaims/ClaimExpansion.java
+++ src/redempt/redclaims/ClaimExpansion.java
@@ -4,7 +4,9 @@ import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
+import redempt.redclaims.claim.Claim;
 import redempt.redclaims.claim.ClaimMap;
+import redempt.redclaims.claim.ClaimStorage;
 
 public class ClaimExpansion extends PlaceholderExpansion {
 
@@ -33,14 +35,16 @@ public class ClaimExpansion extends PlaceholderExpansion {
     @Override
     public String onRequest(OfflinePlayer offlinePlayer, String placeholder) {
         Player player = offlinePlayer.getPlayer();
+        ClaimStorage claimStorage = RedClaims.getInstance().getClaimStorage();
 
         // Check if the player isn't null, because you can't do anything if they are.
 
-        if (player != null) {
+        if (player != null && claimStorage.hasClaim(player.getUniqueId())) {
             // Make the local variables with the values, budget, budget used & location.
             int budget = ClaimLimits.getClaimLimit(player);
             int budgetUsed = redClaims.getClaimStorage().getClaimedBlocks(player.getUniqueId());
             Location location = player.getLocation();
+            Claim claim = ClaimMap.getClaim(location);
 
             // Do the check to see which placeholder the user wants and return the value.
             if (placeholder.equalsIgnoreCase("player_budget")) {
@@ -48,7 +52,9 @@ public class ClaimExpansion extends PlaceholderExpansion {
             } else if (placeholder.equalsIgnoreCase("player_budget_used")) {
                 return Integer.toString(budgetUsed);
             } else if (placeholder.equalsIgnoreCase("claim_player_is_in")) {
-                return ClaimMap.getClaim(location).getName();
+                if (claim != null) {
+                    return claim.getName();
+                }
             }
         }
         return null; // Placeholder is unknown by the Expansion
